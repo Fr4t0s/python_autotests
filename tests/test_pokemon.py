9fec47ec735f2c1774b3a6892c69fc64f3834ead
Fr4t0s <99539900+Fr4t0s@users.noreply.github.com>
2023-04-09 03:44:00 +0300

    mod

--- tests/test_pokemon.py
+++ tests/test_pokemon.py
@@ -4,13 +4,13 @@ Url = "https://pokemonbattle.me:9104/"
 
 # проверка статус-кода запроса информации о тренере
 def test_status_code():
-    response = requests.get (f"{Url}trainers", params = {"trainer_id": "3768"}
+    response = requests.get (f"{Url}trainers", params = {"trainer_id": 3768}
 )
     assert response.status_code == 200
     
 # проверка имени и города тренера
-def test_name():
-    response = requests.get (f"{Url}trainers", params = {"trainer_id": "3768"}
+@pytest.mark.parametrize('key, value', [('trainer_name', 'ЫЫЫЫ'), ('city', 'ЫЫ')])
+def test_pats_of_body(key,value):
+    response = requests.get (f"{Url}trainers", params = {"trainer_id": 3768}
 )
-    assert response.json()['trainer_name'] == 'ЫЫЫЫ'
-    assert response.json()['city'] == 'ЫЫ'
+    assert response.json()[key] ==value
